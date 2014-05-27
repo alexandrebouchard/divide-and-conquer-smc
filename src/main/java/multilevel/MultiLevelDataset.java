f284ef424515276825e15740c08027b57390772e
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-05-26 20:16:59 -0700

    Add node class to include level info and avoid collisions

--- src/main/java/multilevel/MultiLevelDataset.java
+++ src/main/java/multilevel/MultiLevelDataset.java
@@ -15,8 +15,8 @@ import briefj.BriefMaps;
 
 public class MultiLevelDataset
 {
-  private Map<String,Set<String>> children = Maps.newHashMap();
-  private Map<String,Datum> data = Maps.newHashMap();
+  private Map<Node,Set<Node>> children = Maps.newHashMap();
+  private Map<Node,Datum> data = Maps.newHashMap();
   
   public static class Datum
   {
@@ -26,6 +26,53 @@ public class MultiLevelDataset
     {
       this.numberOfTrials = numberOfTrials;
       this.numberOfSuccesses = numberOfSuccesses;
+    }
+  }
+  
+  public static class Node
+  {
+    public final int level;
+    public final String label;
+    
+    private Node(int level, String label)
+    {
+      super();
+      this.level = level;
+      this.label = label;
+    }
+    @Override
+    public int hashCode()
+    {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((label == null) ? 0 : label.hashCode());
+      result = prime * result + level;
+      return result;
+    }
+    @Override
+    public boolean equals(Object obj)
+    {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Node other = (Node) obj;
+      if (label == null)
+      {
+        if (other.label != null)
+          return false;
+      } else if (!label.equals(other.label))
+        return false;
+      if (level != other.level)
+        return false;
+      return true;
+    }
+    @Override
+    public String toString()
+    {
+      return "Level" + level + ":" + label + "]";
     }
   }
   
@@ -43,9 +90,15 @@ public class MultiLevelDataset
       List<String> datum =line.subList(line.size() - 2, line.size());
       
       for (int i = 0; i < path.size() - 1; i++)
-        BriefMaps.getOrPutSet(children, path.get(i)).add(path.get(i+1));
+        BriefMaps.getOrPutSet(children, new Node(i, path.get(i))).add(new Node(i+1, path.get(i+1)));
         
-      data.put(BriefLists.last(path), new Datum(Integer.parseInt(datum.get(0)), Integer.parseInt(datum.get(1))));
+      data.put(new Node(path.size()-1, BriefLists.last(path)), new Datum(Integer.parseInt(datum.get(0)), Integer.parseInt(datum.get(1))));
     }
+  }
+  
+  public static void main(String [] args)
+  {
+    MultiLevelDataset data = new MultiLevelDataset(new File("data/processed/preprocessedNYSData.csv"));
+    System.out.println(data.children);
   }
 }
